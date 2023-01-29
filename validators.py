698b03a9514803b632b7c569647e7be2b75b64af
Tim Nelson <traveltimn@gmail.com>
2023-01-29 13:56:13 +0000

    Add validation helper file

---
+++ validators.py
@@ -0,0 +1,25 @@
+from colors import red, grey
+from utils import clear, LINE, CENT
+
+
+def validate_user_input(user_input, choices):
+    """
+    Checks if the user selected a valid option from the choices provided.
+    If not, it raises a value error.
+    """
+
+    try:
+        if (user_input not in choices):
+            raise ValueError
+    except ValueError:
+        clear()
+        input(
+            red(LINE) + "\n" +
+            red(CENT(f"\"{user_input}\" is not valid!")) + "\n" +
+            grey(CENT("Press ENTER to proceed.")) +
+            "\n" + red(LINE) + "\n"
+        )
+        clear()
+        return False
+
+    return True
