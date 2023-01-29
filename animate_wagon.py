d9e0441c9e02ab865071bcf82e45d9fda4f86a80
Tim Nelson <traveltimn@gmail.com>
2023-01-29 10:46:04 +0000

    Add animate_wagon animation

---
+++ animate_wagon.py
@@ -0,0 +1,224 @@
+import time
+from utils import clear
+
+
+def animate_wagon():
+    print(f"""
+                                                                        ...
+                                                  :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                                  .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                                   :#7 Y@@@@@@#B@@@B#@@@@@@@G
+                                 :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+                                 ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+                                 7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                                   .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                                      ...
+                                                :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                                .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                                 :#7 Y@@@@@@#B@@@B#@@@@@@@G
+                               :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+                               ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+                               7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                                   .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                                    ...
+                                              :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                              .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                               :#7 Y@@@@@@#B@@@B#@@@@@@@G
+                             :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+                             ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+                             7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                                 .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                                  ...
+                                            :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                            .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                             :#7 Y@@@@@@#B@@@B#@@@@@@@G
+                           :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+                           ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+                           7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                               .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                                ...
+                                          :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                          .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                           :#7 Y@@@@@@#B@@@B#@@@@@@@G
+                         :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+                         ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+                         7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                             .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                              ...
+                                        :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                        .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                         :#7 Y@@@@@@#B@@@B#@@@@@@@G
+                       :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+                       ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+                       7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                           .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                            ...
+                                      :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                      .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                       :#7 Y@@@@@@#B@@@B#@@@@@@@G
+                     :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+                     ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+                     7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                         .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                          ...
+                                    :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                    .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                     :#7 Y@@@@@@#B@@@B#@@@@@@@G
+                   :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+                   ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+                   7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                       .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                        ...
+                                  :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                  .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                   :#7 Y@@@@@@#B@@@B#@@@@@@@G
+                 :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+                 ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+                 7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                     .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                      ...
+                                :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                                .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                                 :#7 Y@@@@@@#B@@@B#@@@@@@@G
+               :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+               ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+               7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                   .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                    ...
+                              :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                              .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                               :#7 Y@@@@@@#B@@@B#@@@@@@@G
+             :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+             ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+             7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+                 .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                  ...
+                            :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                            .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                             :#7 Y@@@@@@#B@@@B#@@@@@@@G
+           :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+           ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+           7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+               .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                                ...
+                          :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                          .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                           :#7 Y@@@@@@#B@@@B#@@@@@@@G
+         :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+         ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+         7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+             .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                              ...
+                        :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                        .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                         :#7 Y@@@@@@#B@@@B#@@@@@@@G
+       :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+       ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+       7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+           .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                            ...
+                      :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                      .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                       :#7 Y@@@@@@#B@@@B#@@@@@@@G
+     :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+     ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+     7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+         .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                          ...
+                    :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                    .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                     :#7 Y@@@@@@#B@@@B#@@@@@@@G
+   :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+   ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+   7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+       .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    """)
+    time.sleep(0.25)
+    clear()
+    print(f"""
+                                        ...
+                  :YGGBGGBPJJ!^.~~~.^?JJYGGGBY
+                  .GG!B@@@@@@@#G@@@G#@@@@@@@@P
+                   :#7 Y@@@@@@#B@@@B#@@@@@@@G
+ :^.:7.             !&~ #@@@@@#B@@@B#@@@@@@G:
+ ~5##@J75P557Y5Y!.  .J~:5PPGBG55GGG5PPPGBGG!
+ 7J5BBB&@@@@@@@@G^^^^7J?~?5?JPJJ7JY?~J5?JPJ7
+     .J@@BY5Y7##&5    :~ ^Y?GG5JY.~! ^GJGG55G.
+^^^^~G555^::JB!5&J^^^^:^^7PG?PY~^^:^^!PBJ5P!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    """)
