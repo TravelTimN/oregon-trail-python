81d75c9ce173507947cecb1f26162f09638e3b71
Tim Nelson <traveltimn@gmail.com>
2023-01-29 21:16:50 +0000

    Reformat color docstrings

--- colors.py
+++ colors.py
@@ -8,87 +8,76 @@ def colored(r, g, b, text):
 
 def aqua(text):
     """
-    Changes text to aqua in terminal
-    #00FFFF
+    Changes text to aqua in terminal (#00FFFF)
     """
     return colored(0, 255, 255, text)
 
 
 def blue(text):
     """
-    Changes text to blue in terminal
-    #0000FF
+    Changes text to blue in terminal (#0000FF)
     """
     return colored(0, 0, 255, text)
 
 
 def brown(text):
     """
-    Changes text to brown in terminal
-    #9E6138
+    Changes text to brown in terminal (#9E6138)
     """
     return colored(158, 97, 56, text)
 
 
 def gold(text):
     """
-    Changes text to gold in terminal
-    #D4AF37
+    Changes text to gold in terminal (#D4AF37)
     """
     return colored(212, 175, 55, text)
 
 
 def green(text):
     """
-    Changes text to green in terminal
-    #00FF00
+    Changes text to green in terminal (#00FF00)
     """
     return colored(0, 255, 0, text)
 
 
 def grey(text):
     """
-    Changes text to grey in terminal
-    #BBBBBB
+    Changes text to grey in terminal (#BBBBBB)
     """
     return colored(187, 187, 187, text)
 
 
 def peach(text):
     """
-    Changes text to peach in terminal
-    #FFC7A6
+    Changes text to peach in terminal (#FFC7A6)
     """
     return colored(255, 199, 166, text)
 
 
 def pink(text):
     """
-    Changes text to pink in terminal
-    #FF10F0
+    Changes text to pink in terminal (#FFB6C1)
     """
-    return colored(255, 16, 240, text)
+    return colored(255, 182, 193, text)
 
 
 def red(text):
     """
-    Changes text to red in terminal
-    #FF0000
+    Changes text to red in terminal (#FF0000)
     """
     return colored(255, 0, 0, text)
 
 
 def yellow(text):
     """
-    Changes text to yellow in terminal
-    #FFFF00
+    Changes text to yellow in terminal (#FFFF00)
     """
     return colored(255, 255, 0, text)
 
 
 def white(text):
     """
-    Changes text to white in terminal
-    #FFFFFF
+    Changes text to white in terminal (#FFFFFF)
     """
     return colored(255, 255, 255, text)
