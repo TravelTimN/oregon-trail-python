43484b359e4fcb5c5d0ac2a2217c973cf4160419
Tim Nelson <traveltimn@gmail.com>
2023-01-29 23:11:42 +0000

    Add helper function to validate yes/no, or similar

--- validators.py
+++ validators.py
@@ -2,9 +2,28 @@ from colors import red, grey
 from utils import clear, LINE, CENT
 
 
+def validate_yes_no(user_input):
+    """
+    Checks if the user input is valid (yes/no)
+    """
+    try:
+        if user_input[0].lower() != "y" and user_input[0].lower() != "n":
+            raise ValueError
+    except ValueError:
+        clear()
+        print(red(LINE))
+        print(red(CENT(f'Error: "{user_input}" is not Yes or No!')))
+        print(red(LINE))
+        input(f'{grey(CENT("Press ENTER to proceed."))}\n')
+        clear()
+        return False
+
+    return True
+
+
 def validate_name(user_input):
     """
-    Checks if the user input valid alpha characters (with min/max length)
+    Checks if the user input is valid alpha characters (with min/max length)
     """
     try:
         if not user_input.isalpha() or len(user_input) < 2 or len(user_input) > 15:  # noqa
