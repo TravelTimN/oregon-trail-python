9a51c886a4ea03bf40936b0278708f57bf5abb8c
Tim Nelson <traveltimn@gmail.com>
2023-01-29 13:55:12 +0000

    Add new utilities helper file

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import os
+import datetime
+from colors import brown, peach
 
 
 def clear():
@@ -6,3 +8,22 @@ def clear():
     Clear function to clean-up the terminal so things don't get messy.
     """
     os.system("cls" if os.name == "nt" else "clear")
+
+
+# always ensure text is "center" at 80-chars wide
+CENT = "{:^80}".format
+
+# build a dashed-line exactly 80-chars wide
+LINE = "━" * 80
+
+
+def set_start_date():
+    """
+    Using the player's input, set the start date of
+    the game in 1848 from either March, April, May, June, or July
+    """
+    date = datetime.datetime(1848, 3, 1)
+    date += datetime.timedelta(days=-1)  # set date back to original (-1)
+    for i in range(5):
+        date += datetime.timedelta(days=1)
+        print(date.strftime("%B %d, %Y"))
