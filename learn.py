0c2324b4d043eb1a8baf2b6a514f04f20318f9fc
Tim Nelson <traveltimn@gmail.com>
2023-01-29 18:49:02 +0000

    Build reusable title for learn menu

--- learn.py
+++ learn.py
@@ -1,11 +1,24 @@
-from colors import grey, brown
+from colors import green, grey
 from utils import clear, ot_title, CENT, LINE
 
 
-def learn_about_trail():
-    clear()
-    ot_title()
+def learn_title():
+    """
+    Displays the header text.
+    """
+    print(green(LINE))
+    print(green(CENT('Learn about "The Oregon Trail"')))
+    print(green(LINE))
     print("")
+
+
+def learn_about_trail():
+    """
+    Displays a series of pages that explain a few
+    things about the game play.
+    """
+    clear()
+    learn_title()
     print(CENT("Try taking a journey by covered wagon across"))
     print(CENT("2000 miles of plains, rivers, and mountains."))
     print(CENT("Try!"))
@@ -13,46 +26,42 @@ def learn_about_trail():
     print(CENT("mud and water-filled ruts, or will you plod"))
     print(CENT("through dust six inches deep?"))
     print("")
-    print(brown(LINE))
+    print(green(LINE))
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
 
     clear()
-    ot_title()
-    print("")
+    learn_title()
     print(CENT("How will you cross the rivers?"))
     print(CENT("If you have money, you might take a ferry."))
     print(CENT("(if there is a ferry)"))
     print(CENT("Or, you can ford the river and hope you"))
     print(CENT("and your wagon aren't swallowed alive!"))
     print("")
-    print(brown(LINE))
+    print(green(LINE))
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
 
     clear()
-    ot_title()
-    print("")
+    learn_title()
     print(CENT("What about supplies?"))
     print(CENT("Well, if you're low on food, you can hunt."))
     print(CENT("You might get a buffalo... you might."))
-    print(CENT("And there are bear in the mountains."))
+    print(CENT("And there are bears in the mountains."))
     print("")
-    print(brown(LINE))
+    print(green(LINE))
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
 
     clear()
-    ot_title()
-    print("")
+    learn_title()
     print(CENT("At the Dalles, you can try navigating"))
     print(CENT("the Columbia River, but if running"))
     print(CENT("the rapids with a makeshift raft makes"))
     print(CENT("you queasy, better take the Barlow Road."))
     print("")
-    print(brown(LINE))
+    print(green(LINE))
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
 
     clear()
-    ot_title()
-    print("")
+    learn_title()
     print(CENT("If, for some reason, you don't survive --"))
     print(CENT("your wagon burns, or thieves steal your oxen,"))
     print(CENT("or you run out of provisions, or you die of cholera"))
@@ -60,5 +69,5 @@ def learn_about_trail():
     print(CENT("until your name is up with the others on"))
     print(CENT("The Oregon Top Ten."))
     print("")
-    print(brown(LINE))
+    print(green(LINE))
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
