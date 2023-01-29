00608b86f9b1caad7d851c26d9b774a6dda9fd27
Tim Nelson <traveltimn@gmail.com>
2023-01-29 21:17:57 +0000

    Add information for learning about professions

--- learn.py
+++ learn.py
@@ -1,10 +1,10 @@
-from colors import green, grey
+from colors import green, grey, pink
 from utils import clear, ot_title, CENT, LINE
 
 
-def learn_title():
+def learn_trail_title():
     """
-    Displays the header text.
+    Displays the header text for learning about the trail.
     """
     print(green(LINE))
     print(green(CENT('Learn about "The Oregon Trail"')))
@@ -18,7 +18,7 @@ def learn_about_trail():
     things about the game play.
     """
     clear()
-    learn_title()
+    learn_trail_title()
     print(CENT("Try taking a journey by covered wagon across"))
     print(CENT("2000 miles of plains, rivers, and mountains."))
     print(CENT("Try!"))
@@ -30,7 +30,7 @@ def learn_about_trail():
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
 
     clear()
-    learn_title()
+    learn_trail_title()
     print(CENT("How will you cross the rivers?"))
     print(CENT("If you have money, you might take a ferry."))
     print(CENT("(if there is a ferry)"))
@@ -41,7 +41,7 @@ def learn_about_trail():
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
 
     clear()
-    learn_title()
+    learn_trail_title()
     print(CENT("What about supplies?"))
     print(CENT("Well, if you're low on food, you can hunt."))
     print(CENT("You might get a buffalo... you might."))
@@ -51,7 +51,7 @@ def learn_about_trail():
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
 
     clear()
-    learn_title()
+    learn_trail_title()
     print(CENT("At the Dalles, you can try navigating"))
     print(CENT("the Columbia River, but if running"))
     print(CENT("the rapids with a makeshift raft makes"))
@@ -61,7 +61,7 @@ def learn_about_trail():
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
 
     clear()
-    learn_title()
+    learn_trail_title()
     print(CENT("If, for some reason, you don't survive --"))
     print(CENT("your wagon burns, or thieves steal your oxen,"))
     print(CENT("or you run out of provisions, or you die of cholera"))
@@ -70,4 +70,34 @@ def learn_about_trail():
     print(CENT("The Oregon Top Ten."))
     print("")
     print(green(LINE))
+    input(f'{grey(CENT("Press ENTER to continue"))}\n')
+
+
+def learn_professions_title():
+    """
+    Displays the header text for learning about professions.
+    """
+    print(pink(LINE))
+    print(pink(CENT("Learn about the Professions")))
+    print(pink(LINE))
+    print("")
+
+
+def learn_about_professions():
+    """
+    Displays a series of pages that explain a few
+    things about the three professions.
+    """
+    clear()
+    learn_professions_title()
+    print(CENT("Traveling to Oregon isn't easy!"))
+    print(CENT("But if you're a banker, you'll have more money for supplies"))
+    print(CENT("and services than a carpenter or a farmer."))
+    print("")
+    print(CENT("However, the harder you have to try,"))
+    print(CENT("the more points you deserve!"))
+    print(CENT("Therefore, the farmer earns the greatest number of points,"))
+    print(CENT("and the banker earns the least."))
+    print("")
+    print(pink(LINE))
     input(f'{grey(CENT("Press ENTER to continue"))}\n')
