810346edaa3d1e3cc6f4b83379b68ca898af9eaf
Tim Nelson <traveltimn@gmail.com>
2023-01-29 09:39:52 +0000

    Add clear() fn, and basic starter menu"

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+
+
+def clear():
+    """
+    Clear function to clean-up the terminal so things don't get messy.
+    """
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+if __name__ == "__main__":
+    clear()
+    print("The Oregon Trail")
+    print("-----------------")
+    print("You may:\n")
+    print("\t1. Travel the trail")
+    print("\t2. Learn about the trail")
+    print("\t3. See the Oregon Top Ten")
+    print("\t4. End")
+    input("\nWhat is your choice?")
