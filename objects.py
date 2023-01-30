14d570c53293b4e5569debe7fa67bb4a07139e59
Tim Nelson <traveltimn@gmail.com>
2023-01-30 11:28:46 +0000

    Add helper method for randomizing family names

--- objects.py
+++ objects.py
@@ -1,13 +1,29 @@
+import random
+
+
 class Person:
     """
     A person is an instance of the main player, plus their 4 family members.
     """
+    names = ["Anna", "Beth", "Emily", "Henry", "Jed", "Joey", "John", "Mary", "Sara", "Zeke"]  # noqa
+
     def __init__(self, name):
         self.name = name
         self.illness = None
         self.accident = None
         self.health = "good"
         self.is_alive = True
+
+    def get_random_name(self):
+        """
+        Randomize the list of default names.
+        Select the first name in the shuffled list.
+        Remove that name to avoid duplicates.
+        """
+        random.shuffle(self.names)
+        first_name_selected = self.names[0]
+        self.names.pop(0)
+        return first_name_selected
 
 
 class Inventory:
@@ -34,6 +50,9 @@ class Player(Person):
         self.cash = 0
 
     def profession_starter_cash(self):
+        """
+        Sets the initial cash-value, depending on the player's profession
+        """
         if self.profession == "banker":
             self.cash = int(1600)
         elif self.profession == "carpenter":
