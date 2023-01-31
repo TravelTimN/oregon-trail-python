421c773aca59823ef06b31988aff20a4d275709d
Tim Nelson <traveltimn@gmail.com>
2023-01-31 22:19:40 +0000

    Add starter 'journey' file

---
+++ journey.py
@@ -0,0 +1,17 @@
+import time
+from colors import green, grey
+from objects import Game, Inventory, Person, Player
+from utils import clear, LINE, CENT
+from validators import (
+    validate_choice, validate_yes_no, validate_minmax
+)
+
+
+PLAYER = GAME = INVENTORY = None
+
+
+def landmark_menu():
+    """
+    Generic function that loads the landmark-specific menu options
+    """
+    pass
