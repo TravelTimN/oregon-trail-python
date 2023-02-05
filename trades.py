c7835c7583d4bc2f28d4e098f2506cae511297e6
Tim Nelson <traveltimn@gmail.com>
2023-02-05 13:41:32 +0000

    Add starter values for trading, with weights/ratio

---
+++ trades.py
@@ -0,0 +1,82 @@
+# https://stackoverflow.com/a/41852266
+
+wants_choices = [
+    {
+        "item": "nobody",
+    },
+    {
+        "item": "ox",
+        "qty": 1,
+        "inventory_name": "oxen"
+    },
+    {
+        "item": "set of clothing",
+        "qty": 1,
+        "inventory_name": "clothing"
+    },
+    {
+        "item": "wagon axle",
+        "qty": 1,
+        "inventory_name": "axles"
+    },
+    {
+        "item": "wagon tongue",
+        "qty": 1,
+        "inventory_name": "tongues"
+    },
+    {
+        "item": "wagon wheel",
+        "qty": 1,
+        "inventory_name": "wheels"
+    },
+    {
+        "item": "pounds of food",
+        "qty": 100,
+        "inventory_name": "food"
+    },
+    {
+        "item": "bullets",
+        "qty": 100,
+        "inventory_name": "bullets"
+    },
+    {
+        "item": "wagon tongues",
+        "qty": 2,
+        "inventory_name": "tongues"
+    },
+    {
+        "item": "wagon wheels",
+        "qty": 2,
+        "inventory_name": "wheels"
+    },
+    {
+        "item": "sets of clothing",
+        "qty": 3,
+        "inventory_name": "clothing"
+    },
+    {
+        "item": "wagon wheels",
+        "qty": 3,
+        "inventory_name": "wheels"
+    },
+    {
+        "item": "sets of clothing",
+        "qty": 4,
+        "inventory_name": "clothing"
+    },
+    {
+        "item": "wagon tongues",
+        "qty": 4,
+        "inventory_name": "tongues"
+    },
+    {
+        "item": "wagon wheels",
+        "qty": 4,
+        "inventory_name": "wheels"
+    },
+]
+want_weights = [0.2, 0.1, 0.08, 0.03, 0.16, 0.1, 0.03, 0.07, 0.03, 0.05, 0.02, 0.02, 0.01, 0.01, 0.02]  # noqa
+gender_choices = ["He", "She"]
+gender_weights = [0.75, 0.25]
+gives_choices = ["1 ox", "1 set of clothing", "1 wagon axle", "1 wagon wheel", "bullets", "food"]  # noqa
+gives_weights = [0.12, 0.17, 0.03, 0.01, 0.17, 0.16]
