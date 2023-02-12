620a95f9bf77d204be41db42922088996b598de2
Tim Nelson <traveltimn@gmail.com>
2023-02-12 16:00:58 +0000

    Create global events list of dicts

---
+++ events.py
@@ -0,0 +1,102 @@
+EVENTS = [
+    {
+        "id": 1,
+        "event": "Indians help find food",
+        "weight": 0.05
+    },
+    {
+        "id": 2,
+        "event": "Severe thunderstorm",
+        "weight": 0.2
+    },
+    {
+        "id": 3,
+        "event": "Severe blizzard",
+        "weight": 0.15
+    },
+    {
+        "id": 4,
+        "event": "Heavy fog",
+        "weight": 0.06
+    },
+    {
+        "id": 5,
+        "event": "Hail storm",
+        "weight": 0.06
+    },
+    {
+        "id": 6,
+        "event": "Injured ox",
+        "weight": 0.275
+    },
+    {
+        "id": 7,
+        "event": "Injured party member",
+        "weight": 0.275
+    },
+    {
+        "id": 8,
+        "event": "Snake bite",
+        "weight": 0.007
+    },
+    {
+        "id": 9,
+        "event": "Lose trail",
+        "weight": 0.02
+    },
+    {
+        "id": 10,
+        "event": "Wrong trail",
+        "weight": 0.01
+    },
+    {
+        "id": 11,
+        "event": "Rough trail",
+        "weight": 0.025
+    },
+    {
+        "id": 12,
+        "event": "Impassible trail",
+        "weight": 0.025
+    },
+    {
+        "id": 13,
+        "event": "Find wild fruit",
+        "weight": 0.04
+    },
+    {
+        "id": 14,
+        "event": "Fire in the wagon",
+        "weight": 0.02
+    },
+    {
+        "id": 15,
+        "event": "Lost party member",
+        "weight": 0.01
+    },
+    {
+        "id": 16,
+        "event": "Ox wanders off",
+        "weight": 0.01
+    },
+    {
+        "id": 17,
+        "event": "Find an abandoned wagon",
+        "weight": 0.02
+    },
+    {
+        "id": 18,
+        "event": "Thief comes during the night",
+        "weight": 0.02
+    },
+    {
+        "id": 19,
+        "event": "Illness",
+        "weight": 0.1
+    },
+    {
+        "id": 20,
+        "event": "None",
+        "weight": 0.3
+    }
+]
